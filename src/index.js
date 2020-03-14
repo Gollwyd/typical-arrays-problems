b34f62937dd83ccccdad7310507c3f9d8f0eddb0
Viktor Makarov <gollwyd@mail.ru>
2020-03-14 23:12:17 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    try {
+      let a =  array.length;
+    } catch (error) {
+        return 0;
+    }
+    let num = 0;
+    for (let i=0; i < array.length; i++) {
+        if (array[i] < num) num = array[i];
+    }
+  return num;
 }
 
 exports.max = function max (array) {
-  return 0;
+    try {
+        let a =  array.length;
+      } catch (error) {
+          return 0;
+      }
+    let num = 0;
+    for (let i=0; i < array.length; i++) {
+        if (array[i] > num) num = array[i];
+    }
+  return num;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    try {
+        let a =  array[1];
+      } catch (error) {
+          return 0;
+      }
+    let b = array.length;
+    if (b==0) b = 1;
+    let sum = 0;
+    array.forEach(element => {
+        sum+=element;
+    });
+  return sum/b;
 }
